9fda8aad0354f2f86346c3c40a60fc57692c38b6
E335-comma <ifeoluwaaderoju6@gmail.com>
2025-12-26 19:36:29 +0100

    Add security groups

--- terraform-config/networking.tf
+++ terraform-config/networking.tf
@@ -52,3 +52,41 @@ resource "aws_route_table_association" "public_rt_association" {
   subnet_id      = aws_subnet.public_subnet.id
   route_table_id = aws_route_table.public_rt.id
 }
+
+resource "aws_security_group" "ec2_sg" {
+  name = "adeife-security-group"
+  description = "Allow SSH, HTTP"
+  vpc_id = aws_vpc.vpc.id
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_block  = "0.0.0.0/0"
+  }
+
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_block  = "0.0.0.0/0"
+  }
+
+  ingress {
+    description = "Docker App Port"
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_block  = "0.0.0.0/0"
+  }
+
+  egress {
+    from_port  = 0
+    to_port    = 0
+    protocol   = "-1"
+    cidr_block = "0.0.0.0/0"
+  }
+}
+
