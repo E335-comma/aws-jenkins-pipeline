7e5a1caa850b860fa3822ac6d7d2728094b0dcbb
E335-comma <ifeoluwaaderoju6@gmail.com>
2025-12-26 22:13:01 +0100

    fix security group

--- terraform-config/networking.tf
+++ terraform-config/networking.tf
@@ -63,7 +63,7 @@ resource "aws_security_group" "ec2_sg" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_block  = "0.0.0.0/0"
+    cidr_blocks  = ["0.0.0.0/0"]
   }
 
   ingress {
@@ -71,7 +71,7 @@ resource "aws_security_group" "ec2_sg" {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    cidr_block  = "0.0.0.0/0"
+    cidr_blocks  = ["0.0.0.0/0"]
   }
 
   ingress {
@@ -79,14 +79,14 @@ resource "aws_security_group" "ec2_sg" {
     from_port   = 3000
     to_port     = 3000
     protocol    = "tcp"
-    cidr_block  = "0.0.0.0/0"
+    cidr_blocks  = ["0.0.0.0/0"]
   }
 
   egress {
     from_port  = 0
     to_port    = 0
     protocol   = "-1"
-    cidr_block = "0.0.0.0/0"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
