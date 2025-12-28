d5af1327728258bff4dacd599522a481ec130893
E335-comma <ifeoluwaaderoju6@gmail.com>
2025-12-28 20:39:15 +0100

    fix

--- terraform-config/compute.tf
+++ terraform-config/compute.tf
@@ -5,7 +5,7 @@ resource "tls_private_key" "ssh_key" {
 
 resource "aws_key_pair" "adeife_key" {
   key_name   = "adeife-key"
-  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQD3F6tyPEFEzV0LX3X8BsXdMsQz1x2cEikKDEY0aIj41qgxMCP/iteneqXSIFZBp5vizPvaoIR3Um9xK7PGoW8giupGn+EPuxIA4cDM4vzOqOkiMPhz5XK0whEjkVzTo4+S0puvDZuwIsdiW9mxhJc7tgBNL0cYlWSYVkz4G/fslNfRPW5mYAM49f4fhtxPb5ok4Q2Lg9dPKVHO/Bgeu5woMc7RY0p1ej6D4CKFE6lymSDJpW0YHX/wqE9+cfEauh7xZcG0q9t2ta6F6fmX0agvpFyZo8aFbXeUBr7osSCJNgvavWbM/06niWrOvYX2xwWdhXmXSrbX8ZbabVohBK41 ifeoluwaaderoju6@gmail.com"
+  public_key = tls_private_key.ssh_key.public_key_openssh
 }
 
 resource "aws_instance" "web" {
